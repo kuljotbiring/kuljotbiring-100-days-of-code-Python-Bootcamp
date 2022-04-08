590afb80c19a8251da9dbfc3af39be54bb647385
Kuljot Biring <ksbiring1@gmail.com>
2022-04-07 21:57:28 -0700

    added treasure island project

---
+++ day3/Project: Treasure Island/treasureisland.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = str.lower(input("Which direction would you like to go? Left or Right? "))
+
+if direction != "left":
+    print("You've falled into an endless abyss and died! GAME OVER!!")
+
+else:
+    move = str.lower(input("Would you like to swim or wait?"))
+    if move != "wait":
+        print("You swam in a crocodile pond and got eaten! GAME OVER!!")
+    else:
+        door = str.lower(input("Which door would you like to enter? Red, Yellow or Blue?"))
+        if door == "yellow":
+            print("You have found the treasure! YOU WIN!!")
+        elif door == "red":
+            print("You get engulfed by flames! GAME OVER!!")
+        elif door == "blue":
+            print("You've entered a beast layer and get devoured! GAME OVER!!")
+        else:
+            print("You entered a room that doesn't exist! GAME OVER!!")
