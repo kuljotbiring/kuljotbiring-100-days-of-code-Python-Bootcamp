c3fa6e73e6046bb22d2553a8dea8364397c34a33
Kuljot Biring <ksbiring1@gmail.com>
2022-04-09 12:30:04 -0700

    add while loop to continue asking until solved

--- day7/Project: Hangman/hangman.py
+++ day7/Project: Hangman/hangman.py
@@ -18,18 +18,35 @@ for char in chosen_word:
 
 print(display)
 
-# Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+# Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the
+# letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+solved = False
+
+# Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters
+# in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while solved == False:
+    # Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+    guess = input("Guess a letter: ").lower()
+
+    # Loop through each position in the chosen_word;
+    # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+    # Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    for idx in range(0, len(chosen_word)):
+        if chosen_word[idx] == guess:
+            display[idx] = guess
+
+    # Print 'display' and you should see the guessed letter in the correct position and every other letter
+    # replace with "_".
+    # print(display)
+    print(display)
+
+    # check if no "_" remain ie word solved
+    if "_" in display:
+        continue
+    else:
+        solved = True
+        print("You won!")
 
 
-# Loop through each position in the chosen_word;
-# If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-# Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for idx in range(0, len(chosen_word)):
-    if chosen_word[idx] == guess:
-        display[idx] = guess
-
-# Print 'display' and you should see the guessed letter in the correct position and every other letter
-# replace with "_".
-print(display)
