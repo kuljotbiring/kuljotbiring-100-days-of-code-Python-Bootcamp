749de6e02287ddf0d4bc944739d8d94a15f21f0e
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2022-04-09 23:31:05 -0700

    Update cipher.py

--- day8/Project: Caesar Cipher/cipher.py
+++ day8/Project: Caesar Cipher/cipher.py
@@ -1,65 +1,40 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-
-def encrypt(txt, shft):
-    message = []
-    for char in range(len(txt)):
-        # find the index in alphabet that the current index of the character is located
-        index = alphabet.index(txt[char])
-        # if index goes beyond alphabet range of 25, go back to front of alphabet list
-        if index + shft >= 26:
-            index -= 26
-        # add to message, the letter shifted x spaces over in alphabet
-        message.append(alphabet[index + shft])
-    # change list to string and print it
-    message = ''.join(message)
-    print(f"The encoded message is: {message}")
-
-
-def decrypt(txt, shft):
-    message = []
-    for char in range(len(txt)):
-        # find the index in alphabet that the current index of the character is located
-        index = alphabet.index(txt[char])
-        # if index goes beyond alphabet range of 0, go to the back of alphabet list
-        if index - shft <= -1:
-            index = (index - shft) + 26
-            # add to message, the letter shifted x spaces over in alphabet
-            message.append(alphabet[index])
-        else:
-            # add to message, the letter shifted x spaces over in alphabet
-            message.append(alphabet[index - shft])
-    # change list to string and print it
-    message = ''.join(message)
-    print(f"The decoded message is: {message}")
-
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and
-# print the encrypted text.
-# e.g.
+# Create a function called 'ceaser' that takes the 'text' and 'shift' as inputs.
+# and creates or decodes a cipher using the alphabet and shift to move to different letters
 # plain_text = "hello"
 # shift = 5
 # cipher_text = "mjqqt"
 # print output: "The encoded text is mjqqt"
 
-# #HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+def caesar(txt, shft, dir):
+    message = ""
+    new_index = 0
+    for char in range(len(txt)):
+        # find the index in alphabet that the current index of the character is located
+        index = alphabet.index(txt[char])
+        if dir == "encode":
+            # if index goes beyond alphabet range of 25, go back to front of alphabet list
+            if index + shft >= 26:
+                new_index = index - 26
+            # other-wise simply add to update the index
+            else:
+                new_index = index + shft
+        elif dir == "decode":
+            # if index goes beyond alphabet range of 0, go to the back of alphabet list to get index
+            if index - shft <= -1:
+                new_index = (index - shft) + 26
+            # other-wise simply subtract to update the index
+            else:
+                new_index = index - shft
+        # append character to message string
+        message += alphabet[new_index]
+    # display the output message
+    print(f"The {dir}d message is: {message}")
 
-# #🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-# Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a
-# message.
-
-
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
-else:
-    print("You did not select a proper choice. Re-run the program and try again")
+caesar(text, shift, direction)
