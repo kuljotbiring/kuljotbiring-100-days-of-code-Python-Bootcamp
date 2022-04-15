3666b08d7c8ce9271296579d8daa6a87e4184c45
Kuljot Biring <ksbiring1@gmail.com>
2022-04-14 21:42:16 -0700

    add code example for objects

---
+++ day16/classpractice.py
@@ -0,0 +1,15 @@
+import another_module
+from turtle import Turtle, Screen
+print(another_module.another_variable)
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("Green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
