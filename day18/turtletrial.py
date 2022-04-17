488fb1f5dc5513ed0efafb1293ef424035a99925
Kuljot Biring <ksbiring1@gmail.com>
2022-04-17 00:03:00 -0700

    use Turtle to draw a square

---
+++ day18/turtletrial.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green")
+
+for move in range(4):
+    tim.forward(100)
+    tim.left(90)
+    tim.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# only lets the screen close when clicked
+screen = Screen()
+screen.exitonclick()
