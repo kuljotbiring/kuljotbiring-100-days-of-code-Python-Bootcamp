4acafcf2c98bb06d75638c815cb398283873f7c5
Kuljot Biring <ksbiring1@gmail.com>
2022-04-16 17:53:16 -0700

    add question bank using list of objects

---
+++ day17/Project: Quiz Game/quizgame.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+
+# create question bank - list of question objects
+question_bank = []
+
+
+# use a loop to go through the list of dictionaries
+for question in question_data:
+    # pull out the text and answer from each
+    text = question["text"]
+    answer = question["answer"]
+    # put them in an object
+    new_question = Question(text, answer)
+    # add object to list called question_data
+    question_bank.append(new_question)
+
+print(question_bank)
