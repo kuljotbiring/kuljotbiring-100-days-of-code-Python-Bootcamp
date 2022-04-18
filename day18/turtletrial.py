93811a80be68aaeddd43e2a55c31948648db1388
Kuljot Biring <ksbiring1@gmail.com>
2022-04-17 23:07:29 -0700

    add functionality to draw shapes of x sides and random colors

--- day18/turtletrial.py
+++ day18/turtletrial.py
@@ -1,25 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("turtle")
 tim.color("green")
 
 # make a square using turtle and its methods
-for move in range(4):
-    tim.forward(100)
-    tim.left(90)
-    tim.forward(100)
+# for move in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+#     tim.forward(100)
 
 # make a dotted line
-for move in range(50):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# for move in range(50):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+colors = ["red", "green", "blue", "purple", "orange", "magenta", "black", "deep sky blue", "dim gray"]
 
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
 
 
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 
 
