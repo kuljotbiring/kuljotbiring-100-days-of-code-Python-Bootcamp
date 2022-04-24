ccc572e7a236241013054dcbd477dabddbf58c0c
Kuljot Biring <ksbiring1@gmail.com>
2022-04-23 22:07:44 -0700

    create the snake

--- day20/Project: Snake Game/snakegame.py
+++ day20/Project: Snake Game/snakegame.py
@@ -5,8 +5,17 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+# make snake objects using a for loop. make 3 in a row
 
+# get the starting positions as tuples
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
+# make the pieces of the snake object
+for position in starting_position:
+    snake = Turtle()
+    snake.color("white")
+    snake.shape("square")
+    snake.goto(position)
 
 
 screen.exitonclick()
