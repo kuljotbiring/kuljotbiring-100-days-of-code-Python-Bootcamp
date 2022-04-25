49e78e4ed333959e2a36f8dcccfda560162c5f0c
Kuljot Biring <ksbiring1@gmail.com>
2022-04-24 23:42:53 -0700

    changed colors for visual effects

--- day20and21/Project: Snake Game/scoreboard.py
+++ day20and21/Project: Snake Game/scoreboard.py
@@ -8,7 +8,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.color("white")
+        self.color("deep sky blue")
         self.penup()
         self.goto(0, 270)
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
@@ -19,6 +19,7 @@ class ScoreBoard(Turtle):
 
     def game_over(self):
         self.goto(0, 0)
+        self.color("crimson")
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
