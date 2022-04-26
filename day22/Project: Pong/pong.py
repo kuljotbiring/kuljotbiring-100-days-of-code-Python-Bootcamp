7659122c6030fdef9b1155e3a95ae437ca65be37
Kuljot Biring <ksbiring1@gmail.com>
2022-04-25 21:13:07 -0700

    create screen for pong game

---
+++ day22/Project: Pong/pong.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+# create the screen for the game
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+
+
+
+
+
+
+screen.exitonclick()
