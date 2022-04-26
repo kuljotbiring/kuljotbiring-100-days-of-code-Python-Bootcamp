4c87940a631c3d3cfdbedcf206f8e358f19ce75a
Kuljot Biring <ksbiring1@gmail.com>
2022-04-25 21:48:03 -0700

    create paddle and ability to move it up and down

--- day22/Project: Pong/pong.py
+++ day22/Project: Pong/pong.py
@@ -1,15 +1,41 @@
-from turtle import Screen
+from turtle import Turtle, Screen
+
 
 # create the screen for the game
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.color("white")
+paddle.shape("square")
+paddle.shapesize(stretch_len=1, stretch_wid=5)
+paddle.penup()
+paddle.goto(350, 0)
 
 
+# move the paddle up 20 places. then make it go to new x, y coordinate
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
 
 
+# move the paddle down 20 places. then make it go to new x, y coordinate
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
 
+# make a key stroke listener
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+play_game = True
+
+while play_game:
+    screen.update()
 
 screen.exitonclick()
