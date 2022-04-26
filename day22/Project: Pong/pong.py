623c085217967c4aac537ee77242b68e0ff9610d
Kuljot Biring <ksbiring1@gmail.com>
2022-04-25 23:07:29 -0700

    created a paddle class inheriting from Turtle

--- day22/Project: Pong/pong.py
+++ day22/Project: Pong/pong.py
@@ -1,5 +1,5 @@
-from turtle import Turtle, Screen
-
+from turtle import Screen
+from paddle import Paddle
 
 # create the screen for the game
 screen = Screen()
@@ -8,30 +8,15 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.color("white")
-paddle.shape("square")
-paddle.shapesize(stretch_len=1, stretch_wid=5)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-# move the paddle up 20 places. then make it go to new x, y coordinate
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-# move the paddle down 20 places. then make it go to new x, y coordinate
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 # make a key stroke listener
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 play_game = True
 
