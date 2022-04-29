c82552d8bb6aedbf2127d4dbad3825752759676b
Kuljot Biring <ksbiring1@gmail.com>
2022-04-28 21:43:03 -0700

    finishing touches on code

--- day23/Project: Turtle Crossing/scoreboard.py
+++ day23/Project: Turtle Crossing/scoreboard.py
@@ -6,22 +6,22 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
+        self.level = 1
         self.color("black")
         self.penup()
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.goto(0, 270)
-        self.write(f"LEVEL: {self.score}", align="center", font=FONT)
+        self.goto(-280, 260)
+        self.write(f"LEVEL: {self.level}", align="left", font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
-        self.color("crimson")
+        self.color("black")
         self.write(f"GAME OVER", align="center", font=FONT)
 
     def add_point(self):
-        self.score += 1
+        self.level += 1
         self.clear()
         self.update_scoreboard()
