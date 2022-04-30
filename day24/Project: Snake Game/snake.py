2fadf6eb8ffbcfa2ed3b8026be51c94359d19c9b
Kuljot Biring <ksbiring1@gmail.com>
2022-04-30 13:38:28 -0700

    added previous python code and text file starter for high score

---
+++ day24/Project: Snake Game/snake.py
@@ -0,0 +1,70 @@
+from turtle import Turtle
+# get the starting positions as tuples
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        # make snake objects using a for loop. make 3 in a row
+        # make the pieces of the snake object
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        snake = Turtle()
+        snake.color("dark olive green")
+        snake.shape("square")
+        snake.penup()
+        snake.goto(position)
+        self.segments.append(snake)
+
+    def reset(self):
+        for segment in self.segments:
+            segment.reset()
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # adds a segment to the last position using index slicing
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        # make each of the segments move in reverse order so they move to the piece ahead of them position
+        for segment_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[segment_num - 1].xcor()
+            new_y = self.segments[segment_num - 1].ycor()
+            # get a hold of the last segment and move it where the second to last segment is
+            self.segments[segment_num].goto(new_x, new_y)
+
+        # get the first segment and move it 20 paces
+        self.head.forward(MOVE_DISTANCE)
+
+# each of below methods moves when corresponding key is pressed
+# prevents the snake from going back on itself
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
---
+++ day24/Project: Snake Game/snakegame.py
@@ -0,0 +1,56 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game - Press 'q' to quit")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score_board = ScoreBoard()
+
+# make a key stroke listener
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# make the snake body continuously move
+play_game = True
+
+while play_game:
+    screen.update()
+    time.sleep(.1)
+
+    # get snake to move
+    snake.move()
+
+    # detect collision with food. if collision - make new food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        # add a segment to the snake body
+        snake.extend()
+        score_board.increase_score()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        # snake has hit wall and game over and making a new snake
+        score_board.reset()
+        snake.reset()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        # ignore the head piece
+        if snake.head.distance(segment) < 10:
+            # trigger game over by resetting the score and making a new snake
+            score_board.reset()
+            snake.reset()
+
+screen.exitonclick()
