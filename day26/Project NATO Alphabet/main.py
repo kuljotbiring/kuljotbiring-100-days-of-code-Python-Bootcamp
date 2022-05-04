69e8754c12cabd9d60773a5df49753f610c71b70
Kuljot Biring <ksbiring1@gmail.com>
2022-05-03 22:47:21 -0700

    create NATO alphabet using phonetic words for letters

---
+++ day26/Project NATO Alphabet/main.py
@@ -0,0 +1,40 @@
+import pandas
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+# make the csv file into a data frame
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# make the data frame into a dictionary
+nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
+# print(nato_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# get the user input
+user_input = input("Enter a word: ").upper()
+# make a list of the phonetic words associated with the user input
+phonetic_list = [nato_dict[letter] for letter in user_input]
+print(phonetic_list)
+
