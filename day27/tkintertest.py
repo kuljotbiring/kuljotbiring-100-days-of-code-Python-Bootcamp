02f7e4f013a0235366b60ec5e038b1929bff9dbb
Kuljot Biring <ksbiring1@gmail.com>
2022-05-05 21:32:41 -0700

    added code for various tkinter widgets

--- day27/tkintertest.py
+++ day27/tkintertest.py
@@ -14,6 +14,24 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 # place the label and center it on the screen
 my_label.pack()
+my_label["text"] = "New Text!"
+# or you can change text in lae this way
+my_label.config(text="New Text")
+
+# Entry component - makes a field for entry
+entry = tkinter.Entry(width=10)
+entry.pack()
+
+
+# get button to work using event listener
+def button_clicked():
+    # change text in label by getting the value entered
+    my_label.config(text=entry.get())
+
+
+# create a button
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 # make the window stay open
