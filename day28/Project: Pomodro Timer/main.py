a9162eb11b8ba896fdb1c2a3c73e2167de05cf1f
Kuljot Biring <ksbiring1@gmail.com>
2022-05-06 20:06:10 -0700

    added background, image, and first layer of text

--- day28/Project: Pomodro Timer/main.py
+++ day28/Project: Pomodro Timer/main.py
@@ -20,6 +20,15 @@ LONG_BREAK_MIN = 20
 # create window
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
+# create a canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# get the image to use
+tomato_img = PhotoImage(file="tomato.png")
+# add image to the canvas
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
 
 window.mainloop()
