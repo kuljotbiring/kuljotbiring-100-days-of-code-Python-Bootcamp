f6e696d5a60230c3f3cb0f1ba320c9e336cda107
Kuljot Biring <ksbiring1@gmail.com>
2022-05-17 23:14:06 -0700

    functionality for the app to display random french words on button click

--- day31/Project: Flash Card App/main.py
+++ day31/Project: Flash Card App/main.py
@@ -1,6 +1,22 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# get the data frame
+data = pandas.read_csv("./data/french_words.csv")
+# convert the data frame to a dictionary
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    # pick a random list element from the list
+    current_card = random.choice(to_learn)
+    # change the title word to french
+    canvas.itemconfig(card_title, text="French")
+    # change the current French word to the randomly selected word
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 
 # create window
@@ -14,18 +30,21 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_img = PhotoImage(file="./images/card_front.png")
 # add image to the canvas
 canvas.create_image(400, 263, image=card_img)
-canvas.create_text(400, 150, text="Title:", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="Word:", font=("Ariel", 60, "italic"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "italic"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # create unknown button
 x_image = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=x_image, highlightthickness=0)
+unknown_button = Button(image=x_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 # create known button
 check_image = PhotoImage(file="./images/right.png")
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
+
+# start the card off with a random word
+next_card()
 
 window.mainloop()
