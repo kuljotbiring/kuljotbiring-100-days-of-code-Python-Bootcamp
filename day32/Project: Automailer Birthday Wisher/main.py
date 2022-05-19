920ed503aa424f3729292fe8565be65e8a19a087
Kuljot Biring <ksbiring1@gmail.com>
2022-05-18 23:18:21 -0700

    added code to send emails sucessfully from gmail

---
+++ day32/Project: Automailer Birthday Wisher/main.py
@@ -0,0 +1,17 @@
+import smtplib
+
+# save your own username and password into these variables
+my_email = "myemail@gmail.com"
+password = "abc123"
+
+# create a connection using an SMTP object connecting to provider's SMTP server
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    # make the connection secure - encrypting the message
+    connection.starttls()
+    # log into the account to use
+    connection.login(user=my_email, password=password)
+    # now send the mail
+    connection.sendmail(from_addr=my_email, to_addrs="xyz@yahoo.com", msg="Subject:Hello"
+                                                                          "\n\nPython Email Automation Test")
+
+
