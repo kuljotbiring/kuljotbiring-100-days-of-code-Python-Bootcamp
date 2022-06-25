7629f5cf11fc4d245c26cdea00641825acb0dc29
Kuljot Biring <ksbiring1@gmail.com>
2022-06-25 16:43:02 -0700

    add code to project to determine sunrise and sunset for current location

--- day33/Project: ISS Notification/issnotify.py
+++ day33/Project: ISS Notification/issnotify.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 # make a get request
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -30,3 +31,29 @@ latitude = data["iss_position"]["latitude"]
 iss_position = (longitude, latitude)
 
 print(iss_position)
+
+MY_LAT = 38.637241
+MY_LONG = -121.512604
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+# get the sunrise and sunset from the API and then just grab the first hour using split and list indices
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+# use date time to get the current time
+time_now = datetime.now()
+
+# only get the hour from datetime current time
+print(time_now.hour)
