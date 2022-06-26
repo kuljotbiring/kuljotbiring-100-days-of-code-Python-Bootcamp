c7c5221f3dfd3615d51ca4297999cc2ad0a2c04d
Kuljot Biring <ksbiring1@gmail.com>
2022-06-26 13:21:56 -0700

    add comments to code

--- day34/Project: Quizzler App/data.py
+++ day34/Project: Quizzler App/data.py
@@ -1,14 +1,18 @@
 import requests
 
+# put the parameters in a dictionary to use in GET request
 parameters = {
     "amount": 10,
     "type": "boolean"
 }
 
+# use GET to call the API
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 
+# check for errors
 response.raise_for_status()
+# put the response in the JSON format
 data = response.json()
-
+# get just the information inside a list by accessing the resulting dictionary
 question_data = data["results"]
 
