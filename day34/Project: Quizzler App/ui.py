de2a93a24749b6e6dae5a3f9a9c1acb219c9d7ab
Kuljot Biring <ksbiring1@gmail.com>
2022-06-26 18:12:03 -0700

    add functionality for buttons

--- day34/Project: Quizzler App/ui.py
+++ day34/Project: Quizzler App/ui.py
@@ -26,11 +26,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="./images/true.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_check)
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="./images/false.png")
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_check)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -40,3 +40,9 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_check(self):
+        self.quiz.check_answer("True")
+
+    def false_check(self):
+        self.quiz.check_answer("False")
