d7c441d2956313c12ef766d514260589c14a6acb
Kuljot Biring <ksbiring1@gmail.com>
2022-06-26 18:25:49 -0700

    handle cases when user has gone through all questions

--- day34/Project: Quizzler App/ui.py
+++ day34/Project: Quizzler App/ui.py
@@ -39,9 +39,14 @@ class QuizInterface:
 
     def get_next_question(self):
         self.canvas.config(bg="white")
-        self.score_label.config(text=f"Score: {self.quiz.score}")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_check(self):
         is_correct = self.quiz.check_answer("True")
