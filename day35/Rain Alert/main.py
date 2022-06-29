3f2a84f500f2f3c55f348b4c67f2b4c72d70df29
Kuljot Biring <ksbiring1@gmail.com>
2022-06-28 20:29:53 -0700

    update code to determine if rain is forecasted in next 12 hours

--- day35/Rain Alert/main.py
+++ day35/Rain Alert/main.py
@@ -8,6 +8,7 @@ LONG = -121.527946
 parameters = {
     "lat": LAT,
     "lon": LONG,
+    "exclude": "current,minutely,daily",
     "appid": api_key
 }
 
@@ -17,7 +18,21 @@ response.raise_for_status()
 
 data = response.json()
 
-print(data)
+# place the first 12 results for the hours into a list
+weather_slice = data["hourly"][:12]
+
+will_rain = False
+
+# loop through the list
+for hour in weather_slice:
+    # using id from API to determine if rain or snow
+    condition_code = hour["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+# check if any of those times it is raining and inform user
+if will_rain:
+    print("Bring an umbrella")
 
 
 
