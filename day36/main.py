4033e47cfdea330e989c2a9b74ecf3b0f250dcba
Kuljot Biring <ksbiring1@gmail.com>
2022-07-02 15:55:35 -0700

    add code to get stock prices using API

--- day36/main.py
+++ day36/main.py
@@ -1,24 +1,56 @@
-STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+import requests
+import os
+from datetime import date, timedelta
+
+STOCK_NAME = "AAPL"
+COMPANY_NAME = "Apple Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+stock_api = os.getenv("STOCK_API")
+
+url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={STOCK_NAME}&apikey={stock_api}'
+response = requests.get(url)
+data = response.json()
+
+
+# print(data["Time Series (Daily)"]["2022-07-01"]["4. close"])
+
+# get today's date
+today = date.today()
 
 # # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 # TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g.
 #  [new_value for (key, value) in dictionary.items()]
+# save yesterday's date in a variable
+yesterday = today - timedelta(days=1)
+yesterday_price = float(data["Time Series (Daily)"][str(yesterday)]["4. close"])
 
 # TODO 2. - Get the day before yesterday's closing stock price
+# save yesterday's date in a variable
+day_before_yesterday = today - timedelta(days=2)
+day_before_yesterday_price = float(data["Time Series (Daily)"][str(day_before_yesterday)]["4. close"])
 
 # TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 #  Hint: https://www.w3schools.com/python/ref_func_abs.asp
+price_difference = abs(yesterday_price - day_before_yesterday_price)
 
 # TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
 #  before yesterday.
+is_less = False
+change = day_before_yesterday_price - yesterday_price
+percent_change = change / day_before_yesterday_price * 100
+
+if percent_change < 0:
+    is_less = True
+    percent_change *= -1
 
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if percent_change > 5:
+    print("Get News")
 
 # # STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
