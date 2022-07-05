e452b0b08058c7733ae5ed8560f6e8194d92eda4
Kuljot Biring <ksbiring1@gmail.com>
2022-07-04 22:27:58 -0700

    add code to use natural language API for excercise tracker

--- day38/Project: Workout Tracking/main.py
+++ day38/Project: Workout Tracking/main.py
@@ -1,4 +1,31 @@
+import requests
+
 APP_ID = "b54e4ff0"
 
 API_KEY = "c9dd34d38290af55d6ad636b34ae46fc"
 
+GENDER = "male"
+WEIGHT_KG = 90.7
+HEIGHT_CM = 177.8
+AGE = 40
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+excercise_text = input("Tell me which exercise you did: ")
+
+body = {
+    "query": excercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+response = requests.post(exercise_endpoint, json=body, headers=headers)
+result = response.json()
+print(result)
