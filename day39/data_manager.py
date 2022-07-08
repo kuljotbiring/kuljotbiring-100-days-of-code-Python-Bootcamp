329642c5fe93f3acb83e2d8eb266c841922bafcd
Kuljot Biring <ksbiring1@gmail.com>
2022-07-07 22:55:34 -0700

    .

--- day39/data_manager.py
+++ day39/data_manager.py
@@ -1,3 +1,12 @@
+import requests
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.sheety_url = "https://api.sheety.co/e16bd98df5e37971e300376e218ae6fe/flightDeals/prices"
+
+    def get_google_sheet(self):
+        sheet_response = requests.get(url=self.sheety_url)
+        return sheet_response.json()
+
--- day39/main.py
+++ day39/main.py
@@ -1,1 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager
+
+google_sheet = DataManager()
+
+print(google_sheet.get_google_sheet())
