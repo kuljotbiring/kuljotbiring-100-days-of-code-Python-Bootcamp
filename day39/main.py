7ebbc927565494a6dcef07c62ca51e4c58937102
Kuljot Biring <ksbiring1@gmail.com>
2022-07-08 22:15:11 -0700

    use pretty print to display output

--- day39/main.py
+++ day39/main.py
@@ -1,7 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from pprint import pprint
 
 from data_manager import DataManager
 
 google_sheet = DataManager()
 
-print(google_sheet.get_google_sheet())
+pprint(google_sheet.get_google_sheet())
