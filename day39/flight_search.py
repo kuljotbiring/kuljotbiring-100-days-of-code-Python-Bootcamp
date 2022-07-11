be9818cf4759c9dd28849d09d569bf1858a78237
Kuljot Biring <ksbiring1@gmail.com>
2022-07-10 22:10:13 -0700

    add code to update IAT airport codes using KIWI API

--- day39/flight_search.py
+++ day39/flight_search.py
@@ -1,12 +1,19 @@
 import requests
+import os
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+TEQUILA_API_KEY = os.environ.get('KIWI_API')
 
 
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        # print("get destination codes triggered")
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        response.raise_for_status()
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
