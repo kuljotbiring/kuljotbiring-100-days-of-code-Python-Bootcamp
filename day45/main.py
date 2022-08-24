8ae2fbc5fddc9cb750927f1e84ee8eca1232c814
Kuljot Biring <ksbiring1@gmail.com>
2022-08-23 19:37:12 -0700

    use beautiful soup to parse HTML

--- day45/main.py
+++ day45/main.py
@@ -16,3 +16,21 @@ print(soup.prettify())
 
 # gives first occurrence of anchor tag
 print(soup.a)
+
+# finds all anchor tags
+all_anchor_tags = soup.find_all(name="a")
+
+
+for tag in all_anchor_tags:
+    # get just the names of the anchor tags
+    print(tag.getText())
+    # get href links
+    print(tag.get("href"))
+
+# get a hold by attribute name
+heading = soup.find(name="h1", id="name")
+section_heading = soup.find(name="h3", class_="heading")
+
+# drill down into elements using CSS selectors
+company_url = soup.select_one(selector="p a")
+
