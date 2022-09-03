283463edfb24d07007ef62e2e1f5b0e28ddd1e69
Kuljot Biring <ksbiring1@gmail.com>
2022-09-03 11:16:29 -0700

    able to scrape top 100 website

--- day46/main.py
+++ day46/main.py
@@ -1,3 +1,18 @@
 from bs4 import BeautifulSoup
+import requests
 
-input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+
+URL = f"https://www.billboard.com/charts/hot-100/{year}/"
+
+response = requests.get(URL)
+
+# make the soup from parsing the website
+soup = BeautifulSoup(response.text, "html.parser")
+
+top_100_songs = soup.find_all(name="h3", class_="a-no-trucate")
+
+song_titles = [song.getText().strip() for song in top_100_songs]
+
+print(song_titles)
+
