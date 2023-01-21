d1b8796c9a32cddb5fb3fd75cb4eb812c6cb3fd5
Kuljot Biring <ksbiring1@gmail.com>
2023-01-21 12:28:02 -0800

    updated code to reflect html website changes

--- day47/amzn_price_tracker.py
+++ day47/amzn_price_tracker.py
@@ -2,18 +2,27 @@ import requests
 import lxml
 from bs4 import BeautifulSoup
 
-url = "https://www.amazon.com/Duo-Evo-Plus-esterilizadora-vaporizador/dp/B07W55DDFB/ref=sr_1_4?qid=1597660904"
-header = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
-}
+# find the URL of the product I want to track
+url = "https://www.amazon.com/LG-34UM69G-B-34-Inch-UltraWide-Reduction/dp/B06XFXX5JH/ref=sr_1_19?content-id=amzn1" \
+      ".sym.cdff5709-1ad3-49fb-b3a2-fd145c65c07d%3Aamzn1.sym.cdff5709-1ad3-49fb-b3a2-fd145c65c07d&keywords=lg+34+" \
+      "inch+ultrawide+monitor&pd_rd_r=1530c6a4-af98-4f9c-a49f-9adb3f662c69&pd_rd_w=CUmJD&pd_rd_wg=KX2LF&pf_rd_" \
+      "p=cdff5709-1ad3-49fb-b3a2-fd145c65c07d&pf_rd_r=BHVWFJESSGQM31MVS397&qid=1674326831&sr=8-19"
 
+# get the header information from http://myhttpheader.com/
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:108.0) Gecko/20100101 Firefox/108.0",
+    "Accept-Language": "en-US,en;q=0.5"
+}
+# get the response from the webpage
 response = requests.get(url, headers=header)
 
+# use BS to make the soup with the webpage HTML. Use lxml parser instead of html.parser
 soup = BeautifulSoup(response.content, "lxml")
-print(soup.prettify())
 
-price = soup.find(id="priceblock_ourprice").get_text()
-price_without_currency = price.split("$")[1]
-price_as_float = float(price_without_currency)
-print(price_as_float)
+# print out soup results
+# print(soup.prettify())
+
+price = soup.find(name="span", class_="a-price-whole").get_text()
+# price_without_currency = price.split("$")[1]
+price_as_float = float(price)
+print(f"${price_as_float}")
