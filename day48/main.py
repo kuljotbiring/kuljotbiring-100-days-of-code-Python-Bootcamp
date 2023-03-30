5696087f76ac5a4f11b784d883d63e626dd388ef
Kuljot Biring <ksbiring1@gmail.com>
2023-03-29 18:15:20 -0700

    format getting dollars and cents

--- day48/main.py
+++ day48/main.py
@@ -10,8 +10,9 @@ driver = webdriver.Chrome(service=service)
 # open up a webpage
 driver.get("https://www.amazon.com/346E2CUAE-Frameless-UltraWide-Adjustable-Replacement/dp/B08KFSMGJ8/ref=sr_1_3?crid=QFJYW9STVI3B&keywords=ultrawide+monitor+usb-c&qid=1680136303&rnid=1248877011&s=electronics&sprefix=ultrawide+monitor+usb-c%2Celectronics%2C125&sr=1-3")
 # get the element with the price
-price = driver.find_element(By.CLASS_NAME, value='a-price-whole')
-print(f'${price.text}.99')
+dollars = driver.find_element(By.CLASS_NAME, value='a-price-whole')
+cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
+print(f'${dollars.text}.{cents.text}')
 # close the browser - using driver.quit() will close all tabs
 driver.close()
 
