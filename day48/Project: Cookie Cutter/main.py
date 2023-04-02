924e516f1094d9676540c49d69a469e442bfc1cb
Kuljot Biring <ksbiring1@gmail.com>
2023-04-01 20:54:05 -0700

    working code full run

--- day48/Project: Cookie Cutter/main.py
+++ day48/Project: Cookie Cutter/main.py
@@ -1,20 +1,16 @@
-import selenium
-from selenium import webdriver, common
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
 import time
 from time import sleep
 
 
-def click_cookie():
+def click_cookie(cookie_btn):
     """Find cookies and clicks repeatedly for 5 mins
     it contains a .05 sleep to allow multiple clicks"""
-    cookie = driver.find_element(By.XPATH, '//*[@id="bigCookie"]')
-    end_time = time.time() + 60
-    while time.time() < end_time:
-        sleep(.02)
-        cookie.click()
+    # cookie = driver.find_element(By.XPATH, '//*[@id="bigCookie"]')
+    sleep(.02)
+    cookie_btn.click()
 
 
 def get_score():
@@ -63,7 +59,7 @@ def buy_products():
     cookies = get_score()
     prices = product_prices()
     i = len(prices) - 1
-    while cookies >= prices[0]:
+    while prices and (cookies >= prices[0]):
         if cookies >= prices[i]:
             try:
                 item_to_buy = driver.find_element(By.XPATH, f'//*[@id="product{i}"]')
@@ -86,12 +82,17 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
 driver.maximize_window()
 english = driver.find_element(By.XPATH, '//*[@id="langSelect-EN"]')
 english.click()
+sleep(5)
+cookie = driver.find_element(By.XPATH, '//*[@id="bigCookie"]')
+end_time = time.time() + 300
 
-while True:
-    sleep(5)
-    click_cookie()
+while time.time() < end_time:
+    # save the 5 seconds to check what we can buy
+    end_click_time = time.time() + 10
+    while time.time() < end_click_time:
+        click_cookie(cookie)
     buy_upgrades()
     buy_products()
 
-
-
+cookie_per_s = driver.find_element(By.ID, "cookiesPerSecond").text
+print(cookie_per_s)
